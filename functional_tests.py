43b4a73846bbee3ec781b1e285a0c7c57f5d77cc
FlyShare <flyshare@163.com>
2016-10-08 14:52:01 +0800

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -19,20 +20,31 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
         # 她注意到网页的标题和头部都包含 "To-Do" 这个词
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        head_text = self.browser.find_element_by_tag_name('h1').text
 
         # 应用邀请她输入一个待办事项
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(input.get_attribute(
+            'placeholder'), 'Enter a to-do item')
 
         # 她在一个文本框中输入了 "Buy peacock feathers" (购买孔雀羽毛)
         # 伊迪丝的爱好是使用假蝇做饵钓鱼
+        inputbox.send_keys('Buy peacock feathers')
         #
         # 她按回车键后,页面更新了
         # 待办事项表格中显示了 "1: Buy peacock feathers"
+        inputbox.send_keys('Keys.ENTER')
+
+        table = self.browser.find_element_by_id('id_list_table')    # 如没有找到元素就抛出异常
+        rows = table.find_elements_by_tag_name('tr')    # 没有找到就返回一个空 list
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows))
 
         # 页面中有显示了一个文本框,可以输入其他的待办事项
         # 她输入了 "Use peacock feathers to make a fly" (使用孔雀羽毛做假蝇)
         # 伊迪丝做事很有条理
-        #
+        self.fail('Finish the test!')
+        
         # 页面再次更新,她的清单中显示了这两个待办事项
         #
         # 伊迪丝想知道这个网站是否能记住她的清单
