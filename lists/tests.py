2b922b55e462cc4939428c9d45d7b4e4fe1d5fc7
FlyShare <flyshare@163.com>
2016-10-08 14:06:30 +0800

    First unit test and url mapping, dummy view[极端做法]

--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,11 @@
 from django.test import TestCase    # 是unittest.TestCase 的增强版
+from django.core.urlresolvers import resolve  # resolve[做出决定]
+from lists.views import home_page
 
-# Create your tests here.
 
+class HomePageTest(TestCase):
 
-class SmokeTest(TestCase):
-
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+    def test_root_url_resolves_to_home_page_view(self):
+        """单元测试:是否能把 '/' 解析到 view 中的 home_page()函数"""
+        found = resolve('/')  # 解析 '/',会分配 func
+        self.assertEqual(found.func, home_page)
--- superlists/urls.py
+++ superlists/urls.py
@@ -3,8 +3,13 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
+    
     # url(r'^$', 'superlists.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    # include() 表示把此 url映射 让引入另一个 urls 来映射
+    # url(r'^admin/', include(admin.site.urls)),
+    # '$' 表示空字符
+    url(r'^$', 'lists.views.home_page', name='home_page'),
+    
 )
