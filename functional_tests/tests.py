79ee4bfb0101cc323d1c7f919e9ce2d856a4305a
FlyShare <flyshare@163.com>
2016-10-10 00:16:16 +0800

    chapter-6.3

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -36,9 +36,11 @@ class NewVisitorTest(LiveServerTestCase):
         # 伊迪丝的爱好是使用假蝇做饵钓鱼
         inputbox.send_keys('Buy peacock feathers')
         #
-        # 她按回车键后,页面更新了
+        # 她按回车键后,被带到了一个新的 URL
         # 待办事项表格中显示了 "1. Buy peacock feathers"
         inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/list/.+')
 
         # table = self.browser.find_element_by_id(
         # 'id_list_table')    # 如没有找到元素就抛出异常
@@ -58,11 +60,37 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # 页面再次更新,她的清单中显示了这两个待办事项
-        self.check_for_row_in_list_table('1. Buy peacock feathers')
         self.check_for_row_in_list_table(
             '2. Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('1. Buy peacock feathers')
         # 伊迪丝想知道这个网站是否能记住她的清单
-        # 她看到网站为她生成了一个唯一的 URL
-        # 而且页面中有一些文字解说这个功能
+
+        # 现在一个叫 '弗朗西斯' 的新用户访问了网站
+
+        # 我们使用了一个新浏览器会话
+        # 确保 '伊迪丝' 的信息不会从 cookie中泄露出来
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # '弗朗西斯' 输入了一个新待办事项,新建了一个清单
+        # 她不像 '伊迪丝' 那样兴趣盎然
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        # '弗朗西斯' 获得了他的唯一 URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # 这个页面还是没有 '伊迪丝' 的清单
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # 两人都很满意, 睡觉去了
         self.fail('Finish the test!')
-        # 她访问了这个 URL,发现她的待办事项列表还在
