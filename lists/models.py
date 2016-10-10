9d25dc60d707a6fbb963c743f1c39985a6ea3d01
FlyShare <flyshare@163.com>
2016-10-10 14:50:13 +0800

    调整模型, 让待办事项和不同的清单关联起来

--- lists/models.py
+++ lists/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+class List(models.Model):
+    pass
 
 class Item(models.Model):
     """
@@ -7,3 +9,8 @@ class Item(models.Model):
     默认情况,ID 字段会自动生成,其他字段需要手动添加
     """
     text = models.TextField(default='')
+    # 让 Item 的list 字段的类型 跟 List() 对象 关联起来 这里就不是单纯的 text 类型了
+    list = models.ForeignKey(List, default=None)
+
+
+
--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-from lists.models import Item
+from lists.models import Item, List
 
 
 def home_page(request):
@@ -15,6 +15,8 @@ def view_list(request):
     items = Item.objects.all()  # items 传递给 render() 会自动替换 HTML 中的 Python变量  items
     return render(request, 'list.html', {'items': items, })
 
+
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/the-only-list-in-the-world/')
