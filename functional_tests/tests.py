d6fedbca7f89df55973644eb5722c5f9ae6e1f2b
FlyShare <flyshare@163.com>
2016-10-11 11:53:39 +0800

    Hack FT runner to be able to test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,24 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import sys
 
 
 class NewVisitorTest(StaticLiveServerTestCase):
     """"""
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -19,11 +33,11 @@ class NewVisitorTest(StaticLiveServerTestCase):
         rows = self.browser.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
-    def _test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_and_retrieve_it_later(self):
         """"""
         # 伊迪丝听说有一个很酷的在线待办事项应用
         # 她去看了这个应用的首页
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         # 她注意到网页的标题和头部都包含 "To-Do" 这个词
         self.assertIn('To-Do', self.browser.title)
         head_text = self.browser.find_element_by_tag_name('h1').text
@@ -42,6 +56,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
         edith_list_url = self.browser.current_url
         self.assertRegex(edith_list_url, '/lists/.+')
+        self.check_for_row_in_list_table('1. Buy peacock feathers')
 
         # table = self.browser.find_element_by_id(
         # 'id_list_table')    # 如没有找到元素就抛出异常
@@ -61,8 +76,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # 页面再次更新,她的清单中显示了这两个待办事项
-        self.check_for_row_in_list_table(
-            '2. Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('2. Use peacock feathers to make a fly')
         self.check_for_row_in_list_table('1. Buy peacock feathers')
         # 伊迪丝想知道这个网站是否能记住她的清单
 
@@ -72,7 +86,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         # 确保 '伊迪丝' 的信息不会从 cookie中泄露出来
         self.browser.quit()
         self.browser = webdriver.Firefox()
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
@@ -94,11 +108,11 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertNotIn('make a fly', page_text)
 
         # 两人都很满意, 睡觉去了
-        self.fail('Finish the test!')
+        #self.fail('Finish the test!')
 
     def test_layout_and_styling(self):
         # 伊迪丝 访问首页
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
         # 她看到了输入框完美地居中显示
